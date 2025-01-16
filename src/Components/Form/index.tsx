6ff05fe4b80325caca3dc906ce04147acb96c340
ouzymaulana <ouzymaulana@gmail.com>
2025-01-16 10:32:06 +0700

    create alert when submit successfully

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -9,13 +9,12 @@ const Form = () => {
   });
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const [isPasswordShow, setIsPasswordShow] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
-  // Fungsi untuk menangani perubahan input
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormValue({ ...formValue, [name]: value });
 
-    // Hapus pesan error untuk field yang sedang diisi
     if (errors[name]) {
       setErrors((prevErrors) => {
         const newErrors = { ...prevErrors };
@@ -25,7 +24,6 @@ const Form = () => {
     }
   };
 
-  // Fungsi validasi
   const validate = () => {
     const newErrors: { [key: string]: string } = {};
     if (!formValue.name) newErrors.name = "Name is required";
@@ -35,59 +33,65 @@ const Form = () => {
     return newErrors;
   };
 
-  // Fungsi untuk submit form
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     const newErrors = validate();
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
     } else {
-      alert("Form submitted successfully!");
+      setIsSuccess(true);
       setFormValue({ name: "", email: "", password: "", confirmPassword: "" });
       setErrors({});
     }
   };
 
   return (
-    <div className='flex justify-center items-cente'>
-    <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
-        <div className="flex items-center justify-center mb-4">
-            <h5 className="text-xl font-bold leading-none text-gray-900 ">FORM</h5>
-      </div>
-      <form className="flow-root" onSubmit={handleSubmit}>
-        <div className="mb-2">
-            <label className="block mb-2 text-sm font-medium text-gray-900">Name <span className='text-red-600'>*</span></label>
-            <input type="text" name="name" onChange={handleChange} value={formValue.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
-            {errors.name && <small className="mt-2 text-red-600">{errors.name}</small>}
-        </div>
-        <div className="mb-2">
-            <label className="block mb-2 text-sm font-medium text-gray-900">Email</label>
-            <input type="text" name="email" onChange={handleChange} value={formValue.email} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
-            {errors.email && <small className="mt-2 text-red-600">{errors.email}</small>}
-        </div>
-        <div className="mb-2">
-            <label className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-            <div className='relative'>
-              <input type={isPasswordShow ? "text" : "password"} name="password" onChange={handleChange} value={formValue.password} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
-              {!isPasswordShow ? (
-                <span className="material-icons absolute text-base right-4 top-1/4 cursor-pointer" onClick={() => setIsPasswordShow(!isPasswordShow)}>visibility</span>
-              ) : (
-                <span className="material-icons absolute text-base right-4 top-1/4 cursor-pointer" onClick={() => setIsPasswordShow(!isPasswordShow)}>visibility_off</span>
-              )}
+    <>
+      <div className='flex justify-center items-cente'>
+        <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8">
+            <div className="flex items-center justify-center mb-4">
+                <h5 className="text-xl font-bold leading-none text-gray-900 ">FORM</h5>
+          </div>
+          <form className="flow-root" onSubmit={handleSubmit}>
+            <div className="mb-2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Name <span className='text-red-600'>*</span></label>
+                <input type="text" name="name" onChange={handleChange} value={formValue.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                {errors.name && <small className="mt-2 text-red-600">{errors.name}</small>}
             </div>
-            {errors.password && <small className="mt-2 text-red-600">{errors.password}</small>}
+            <div className="mb-2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                <input type="text" name="email" onChange={handleChange} value={formValue.email} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                {errors.email && <small className="mt-2 text-red-600">{errors.email}</small>}
+            </div>
+            <div className="mb-2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                <div className='relative'>
+                  <input type={isPasswordShow ? "text" : "password"} name="password" onChange={handleChange} value={formValue.password} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                  {!isPasswordShow ? (
+                    <span className="material-icons absolute text-base right-4 top-1/4 cursor-pointer" onClick={() => setIsPasswordShow(!isPasswordShow)}>visibility</span>
+                  ) : (
+                    <span className="material-icons absolute text-base right-4 top-1/4 cursor-pointer" onClick={() => setIsPasswordShow(!isPasswordShow)}>visibility_off</span>
+                  )}
+                </div>
+                {errors.password && <small className="mt-2 text-red-600">{errors.password}</small>}
+            </div>
+            <div className="mb-2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
+                <input type="password" name="confirmPassword" onChange={handleChange} value={formValue.confirmPassword} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                {errors.confirmPassword && <small className="mt-2 text-red-600">{errors.confirmPassword}</small>}
+            </div>
+            <div className="mt-4">
+              <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full">Submit</button>
+            </div>
+          </form>
         </div>
-        <div className="mb-2">
-            <label className="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
-            <input type="password" name="confirmPassword" onChange={handleChange} value={formValue.confirmPassword} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
-            {errors.confirmPassword && <small className="mt-2 text-red-600">{errors.confirmPassword}</small>}
+      </div>
+      {isSuccess && (
+        <div className="p-4 w-max mb-4 text-sm text-green-800 rounded-lg bg-green-50 flex items-center gap-4 absolute right-6 bottom-0" data-aos="fade-down">
+          <span className="font-medium">Form Submitted Successfully!!</span><span onClick={() => setIsSuccess(false)} className='material-icons cursor-pointer'>close</span>
         </div>
-        <div className="mt-4">
-          <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 w-full">Submit</button>
-        </div>
-      </form>
-    </div>
-  </div>
+      )}
+    </>
   )
 }
 
--- src/Components/Layout/index.tsx
+++ src/Components/Layout/index.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className='bg-homePage h-screen w-100 bg-cover'>
       <Navbar/>
-      <div className="container h-5/6 mx-auto p-4">
+      <div className="container h-5/6 mx-auto p-4 relative">
         <Outlet />
       </div>
     </div>
