45ee6df4d2bf789a0784bbc2e9205e4581afa85a
ouzymaulana <ouzymaulana@gmail.com>
2025-01-20 13:23:15 +0700

    feat : create loading and show when fetching data on progres

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Swal from 'sweetalert2';
 
 const Form = () => {
   const [formValue, setFormValue] = useState({
@@ -9,7 +10,6 @@ const Form = () => {
   });
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
   const [isPasswordShow, setIsPasswordShow] = useState(false);
-  const [isSuccess, setIsSuccess] = useState(false);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -28,7 +28,8 @@ const Form = () => {
     const newErrors: { [key: string]: string } = {};
     if (!formValue.name) newErrors.name = "Name is required";
     if (!/\S+@\S+\.\S+/.test(formValue.email)) newErrors.email = "Invalid email address";
-    if (formValue.password.length < 8) newErrors.password = "Password must be at least 8 characters";
+    // if (formValue.password.length < 8) newErrors.password = "Password must be at least 8 characters";
+    if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/.test(formValue.password)) newErrors.password = "Password must be at least 8 characters";
     if (formValue.password !== formValue.confirmPassword) newErrors.confirmPassword = "Passwords do not match";
     return newErrors;
   };
@@ -39,9 +40,14 @@ const Form = () => {
     if (Object.keys(newErrors).length > 0) {
       setErrors(newErrors);
     } else {
-      setIsSuccess(true);
       setFormValue({ name: "", email: "", password: "", confirmPassword: "" });
       setErrors({});
+
+      Swal.fire({
+        title: "Successfully Submitted Data!",
+        icon: "success",
+        confirmButtonColor: "#006BFF"
+      });
     }
   };
 
@@ -55,18 +61,18 @@ const Form = () => {
           <form className="flow-root" onSubmit={handleSubmit}>
             <div className="mb-2">
                 <label className="block mb-2 text-sm font-medium text-gray-900">Name <span className='text-red-600'>*</span></label>
-                <input type="text" name="name" onChange={handleChange} value={formValue.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                <input type="text" name="name" onChange={handleChange} value={formValue.name} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5 ${errors.name && 'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500'}`}/>
                 {errors.name && <small className="mt-2 text-red-600">{errors.name}</small>}
             </div>
             <div className="mb-2">
                 <label className="block mb-2 text-sm font-medium text-gray-900">Email</label>
-                <input type="text" name="email" onChange={handleChange} value={formValue.email} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                <input type="text" name="email" onChange={handleChange} value={formValue.email} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${errors.email && 'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500'}`}/>
                 {errors.email && <small className="mt-2 text-red-600">{errors.email}</small>}
             </div>
             <div className="mb-2">
                 <label className="block mb-2 text-sm font-medium text-gray-900">Password</label>
                 <div className='relative'>
-                  <input type={isPasswordShow ? "text" : "password"} name="password" onChange={handleChange} value={formValue.password} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                  <input type={isPasswordShow ? "text" : "password"} name="password" onChange={handleChange} value={formValue.password} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${errors.password && 'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500'}`}/>
                   {!isPasswordShow ? (
                     <span className="material-icons absolute text-base right-4 top-1/4 cursor-pointer" onClick={() => setIsPasswordShow(!isPasswordShow)}>visibility</span>
                   ) : (
@@ -77,7 +83,7 @@ const Form = () => {
             </div>
             <div className="mb-2">
                 <label className="block mb-2 text-sm font-medium text-gray-900">Confirm Password</label>
-                <input type="password" name="confirmPassword" onChange={handleChange} value={formValue.confirmPassword} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                <input type="password" name="confirmPassword" onChange={handleChange} value={formValue.confirmPassword} className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${errors.confirmPassword && 'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:border-red-500 focus:ring-red-500'}`}/>
                 {errors.confirmPassword && <small className="mt-2 text-red-600">{errors.confirmPassword}</small>}
             </div>
             <div className="mt-4">
@@ -86,11 +92,6 @@ const Form = () => {
           </form>
         </div>
       </div>
-      {isSuccess && (
-        <div className="p-4 w-max mb-4 text-sm text-green-800 rounded-lg bg-green-50 flex items-center gap-4 absolute right-6 bottom-0" data-aos="fade-down">
-          <span className="font-medium">Form Submitted Successfully!!</span><span onClick={() => setIsSuccess(false)} className='material-icons cursor-pointer'>close</span>
-        </div>
-      )}
     </>
   )
 }
